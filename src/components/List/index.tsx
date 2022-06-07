0970d21e6081a72eb1798696b015fd1624a55443
Joao Gabriel <joaogabriel1598@gmail.com>
2022-06-07 14:43:43 -0300

    Render array

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 
 function List() {
+    const tasks = [{
+        task: 'React',
+        time: '02:00:00'
+    }, {
+        task: 'Javascript',
+        time: '01:00:00'
+    }, {
+        task: 'Typescript',
+        time: '03:00:00'
+    }]
     return (
         <aside>
             <h2>Tasks of the day</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        JavaScript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tasks.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.task} </h3>
+                        <span> {item.time} </span>
+                    </li>
+                ))}
             </ul>
-
         </aside>
     )
 }

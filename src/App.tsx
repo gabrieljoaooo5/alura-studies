f6d8f381fe518874cffb6f78b9bd6c9c3ca9f639
Joao Gabriel <joaogabriel1598@gmail.com>
2022-06-07 13:57:37 -0300

    Create Button, Form and List components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Form from './components/Forms';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
-     Hello World
+     <Form />
+     <List />
     </div>
   );
 }

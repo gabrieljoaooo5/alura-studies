6f0a3a537089d369b61ec97d3ccb919797c4114c
Joao Gabriel <joaogabriel1598@gmail.com>
2022-06-20 20:18:12 -0300

    Show the timer

--- src/components/timer/Watch/index.tsx
+++ src/components/timer/Watch/index.tsx
@@ -1,13 +1,22 @@
 import style from './Watch.module.scss';
 
-export default function Watch() {
+interface Props {
+    time: Number | undefined
+}
+
+export default function Watch({ time = 0 }: Props) {
+    const minutes = Math.floor(time.valueOf() / 60);
+    const seconds = time.valueOf() % 60;
+    const [minuteTen, minuteOne] = String(minutes).padStart(2, '0');
+    const [secondTen, secondOne] = String(seconds).padStart(2, '0');
+
     return (
        <>
-        <span className={style.numberWatch}>0</span>
-        <span className={style.numberWatch}>0</span>
+        <span className={style.numberWatch}>{minuteTen}</span>
+        <span className={style.numberWatch}>{minuteOne}</span>
         <span className={style.division}>:</span>
-        <span className={style.numberWatch}>0</span>
-        <span className={style.numberWatch}>0</span>
+        <span className={style.numberWatch}>{secondTen}</span>
+        <span className={style.numberWatch}>{secondOne}</span>
        </> 
     )
 }
--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -3,17 +3,26 @@ import Watch from './Watch';
 import style from './Timer.module.scss';
 import { TimeToSeconds } from '../../common/utils/time';
 import { ITask } from '../../types/tasks';
+import { useEffect, useState } from 'react';
 
 interface Props {
     selected: ITask | undefined
 }
 
 export default function Timer({ selected }: Props) {
+    const [time, setTime] = useState<number>();
+
+    useEffect(() => {
+        if(selected?.time) {
+            setTime(TimeToSeconds(selected.time));
+        }
+    }, [selected])
+
     return (
         <div className={style.timer}>
             <p className={style.title}>Choose a card and start the timer</p>
             <div className={style.watchWrapper}>
-                <Watch />
+                <Watch time={time} />
             </div>
             <Button>
                 Start
